c771b46edb37dde509d521eb7569496a7306b619
SuprajaBhat <68591324+SuprajaBhat@users.noreply.github.com>
2020-07-23 19:54:37 +0530

    Add files via upload

---
+++ Dya4 assignments.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+str=input("Enter the string")
+print(str)
+sub=input("Enter the substring")
+print(sub)
+count=str.count(sub)
+print(count)
+
+
+# In[ ]:
+
+
+str=input("Enter the string")
+sub=input("Enter the substring")
+count=str.count(substring)
+print(count)
+
+
+# In[ ]:
+
+
+str=input("Enter the string")
+print(str)
+sub=input("Enter the substring")
+print(sub)
+count=str.count(sub)
+print(count)
+
+
+# In[1]:
+
+
+str=input("Enter the string")
+print(str)
+sub=input("Enter the substring")
+print(sub)
+count=str.count(sub)
+print(count)
+
+
+# In[3]:
+
+
+str=input("Enter the string : ")
+print(str)
+sub=input("Enter the substring : ")
+print(sub)
+count=str.count(sub)
+print(count)
+
+
+# In[4]:
+
+
+str=input("Enter the string")
+print(str)
+sub=input("Enter the substring")
+print(sub)
+count=str.count(sub)
+print(count)
+
+
+# In[8]:
+
+
+str1=SUPRAJA
+Isupper(str1)
+
+
+# In[10]:
+
+
+help(isupper)
+
+
+# In[12]:
+
+
+str=supraja;
+str.isupper()
+
+
+# In[13]:
+
+
+str="String"
+str.isupper()
+
+
+# In[17]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[18]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[19]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[20]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[21]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[22]:
+
+
+str=input("Enter any string : ")
+str.isupper()
+
+
+# In[ ]:
+
+
+
+
